a1e13b0325dd17368e6f41d9ecb8e173b98d592a
Laurent Domb <ldomb@redhat.com>
2015-11-07 21:50:00 -0500

    added 1_customize_image.sh

--- get_all_files.sh
+++ get_all_files.sh
@@ -1,9 +1,12 @@
 cd /home/stack
+
+wget --no-check-certificate https://raw.githubusercontent.com/ldomb/ospdirectorinstaller/master/1_customize_image.sh
 wget --no-check-certificate https://raw.githubusercontent.com/ldomb/ospdirectorinstaller/master/2_createflavors.sh
 wget --no-check-certificate https://raw.githubusercontent.com/ldomb/ospdirectorinstaller/master/3_neutrondns.sh
 wget --no-check-certificate https://raw.githubusercontent.com/ldomb/ospdirectorinstaller/master/4_optimize.sh
 wget --no-check-certificate https://raw.githubusercontent.com/ldomb/ospdirectorinstaller/master/5_registernodes.sh
 wget --no-check-certificate https://raw.githubusercontent.com/ldomb/ospdirectorinstaller/master/7_getyamls.sh
+chmod +x 1_customize_image.sh
 chmod +x 2_createflavors.sh
 chmod +x 3_neutrondns.sh
 chmod +x 4_optimize.sh
